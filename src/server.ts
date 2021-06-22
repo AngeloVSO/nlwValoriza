40ccb81a50bedd7ce79e1966e2d1a490d8a4fe31
AngeloVSO <angelo_odwyer@hotmail.com>
2021-06-21 22:55:32 -0300

    Regras de negócio

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
+import 'reflect-metadata'
 import express from 'express'
+import "./database"
 
 export const app = express()
 
 app.use(express.json())
-
-app.get("/test", (request, response) => {
-    response.send("Olá, Bahia!!!")
-})
-
-app.post("/test-post", (request, response) => {
-    response.send("Test post Bahia")
-})
 
 app.listen(3003, () => {
     console.log("Server is running")
