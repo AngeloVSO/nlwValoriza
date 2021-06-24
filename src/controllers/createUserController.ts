cace318b2a393d305aeb1b25417c5aa32e5f9dc8
AngeloVSO <angelo_odwyer@hotmail.com>
2021-06-24 14:12:31 -0300

    Tratando erro com express-async-erros

--- src/controllers/createUserController.ts
+++ src/controllers/createUserController.ts
@@ -3,7 +3,6 @@ import { CreateUserService } from "../services/createUserServices";
 
 export class CreateUserController {
   async handle(req: Request, res: Response) {
-    try {
       const { name, email, admin } = req.body;
 
       const createUserService = new CreateUserService();
@@ -11,8 +10,5 @@ export class CreateUserController {
       const user = await createUserService.execute({ name, email, admin });
 
       return res.json(user);
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
-import 'reflect-metadata'
-import express from 'express'
-import "./database"
-import { router } from './routes'
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "./database";
+import { router } from "./routes";
 
-export const app = express()
+export const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
+app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return res.status(400).send({ message: error.message });
+  }
+
+  return res
+    .status(500)
+    .send({ status: "error", message: "Internal server error" });
+});
 
 app.listen(3003, () => {
-    console.log("Server is running")
-})
+  console.log("Server is running");
+});
