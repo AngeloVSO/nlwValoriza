dfab3937a15702ceef8dbb2746cf40bf42da5c08
AngeloVSO <angelo_odwyer@hotmail.com>
2021-06-25 23:26:17 -0300

    Criação de endpoints do compliment

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/authenticateUserContorller'
+import { CreateComplimentController } from './controllers/createComplimentController'
 import { CreateTagController } from './controllers/createTagController'
 import { CreateUserController } from './controllers/createUserController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
@@ -9,7 +10,9 @@ export const router = Router()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController
+const createComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin , createTagController.handle)
 router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
